97c25f263ac8d9599bc5ec2cb81189471279c5dd
Jesse Feng <fxm0016@126.com>
2020-05-31 16:42:55 +0800

    Update index.js

---
+++ packages/babel-plugin-jsx-advanced/index.js
@@ -0,0 +1,190 @@
+'use strict';
+
+const parseIfTag = require('./parse/if');
+const parseIfDirective = require('./parse/i-if');
+const parseClassDirective = require('./parse/i-class');
+const parseShowDirective = require('./parse/i-show');
+const parseHtmlDirective = require('./parse/i-html');
+
+const { TAG_IF, TAG_ELSE_IF, TAG_ELSE } = parseIfTag;
+
+function getDirectiveNames({
+  prefix = 'i-',
+  supportIf,
+  supportClass,
+  supportShow,
+  supportHtml
+}) {
+  const names = Object.create(null);
+  if (supportIf !== false) {
+    names.ifDirective = `${prefix}if`;
+    names.elifDirective = `${prefix}elif`;
+    names.elseDirective = `${prefix}else`;
+  }
+  if (supportClass !== false) {
+    names.classDirective = `${prefix}class`;
+    names.classNameAttr = 'className';
+  }
+  if (supportShow !== false) {
+    names.showDirective = `${prefix}show`;
+    names.styleAttr = 'style';
+  }
+  if (supportHtml !== false) {
+    names.htmlDirective = `${prefix}html`;
+    names.dangerouslySetInnerHTMLAttr = 'dangerouslySetInnerHTML';
+  }
+  return names;
+}
+
+function getDirectiveNodes(types, directives, attributes) {
+  const directiveNodes = Object.create(null);
+  for (let i = 0, l = attributes.length; i < l; i++) {
+    const attr = attributes[i];
+    // 有可能存在 JSXSpreadAttribute
+    if (types.isJSXAttribute(attr)) {
+      const { name: { name }, value } = attr;
+      for (const alias in directives) {
+        if (name === directives[alias]) {
+          directiveNodes[alias] = {
+            attr,
+            name,
+            key: i,
+            value: types.isJSXExpressionContainer(value)
+              ? value.expression
+              : value
+          };
+          break;
+        }
+      }
+    }
+  }
+  return directiveNodes;
+}
+
+module.exports = function ({ types }, options) {
+  const directiveNames = getDirectiveNames(options);
+  const {
+    classHelper = 'babel-plugin-jsx-advanced-helper/es/class-helper',
+    showHelper = 'babel-plugin-jsx-advanced-helper/es/show-helper',
+    classHelperAlias = '__classHelper__',
+    showHelperAlias = '__showHelper__',
+    supportIfTag
+  } = options;
+
+  return {
+    visitor: {
+      Program: {
+        enter(nodePath, state) {
+          // 指令辅助函数, key为声明变量名，value为模块名
+          state.__directiveHelpers = new Map();
+        },
+        exit(nodePath, state) {
+          const { body } = nodePath.node;
+          const { __directiveHelpers } = state;
+          for (const [alias, helper] of __directiveHelpers) {
+            if (!body.some(node => node.type === 'ImportDeclaration' && node.specifiers.some(specifier => specifier.local.name === alias))) {
+              // 'import alias from helper';
+              const importDeclaration = types.importDeclaration(
+                [
+                  types.ImportDefaultSpecifier(types.identifier(alias))
+                ],
+                types.stringLiteral(helper)
+              );
+              nodePath.unshiftContainer('body', importDeclaration);
+            }
+          };
+        }
+      },
+      JSXElement(nodePath, state) {
+        const { node: { openingElement: { attributes, name: { name: tagName } } } } = nodePath;
+
+        // 匹配if标签
+        if (supportIfTag !== false) {
+          switch (tagName) {
+            case TAG_IF:
+              parseIfTag(nodePath);
+              return;
+            case TAG_ELSE_IF:
+              throw nodePath.buildCodeFrameError(`<${TAG_ELSE_IF}> 标签需要上一个JSX节点是 '${TAG_IF}' 标签.`);
+            case TAG_ELSE:
+              throw nodePath.buildCodeFrameError(`<${TAG_ELSE}> 标签需要上一个JSX节点是 '${TAG_IF}' 或 '${TAG_ELSE_IF}' 标签.`);
+          }
+        }
+
+        // 匹配需要处理的指令
+        const directiveNodes = getDirectiveNodes(types, directiveNames, attributes);
+        const {
+          ifDirective,
+          elifDirective,
+          elseDirective,
+          classDirective,
+          showDirective,
+          htmlDirective
+        } = directiveNodes;
+
+        let removedNum = 0;
+
+        // 处理if指令
+        if (ifDirective) {
+          if (ifDirective.value === null) {
+            throw nodePath.get('openingElement').get(`attributes.${ifDirective.key}`).buildCodeFrameError(`'${ifDirective.name}' 指令需要绑定一个变量或表达式.`);
+          }
+          parseIfDirective(
+            nodePath,
+            ifDirective,
+            directiveNames.elifDirective,
+            directiveNames.elseDirective
+          );
+          removedNum = attributes.splice(ifDirective.key, 1).length;
+        } else if (elifDirective) {
+          throw nodePath.buildCodeFrameError(`'${elifDirective.name}' 指令需要上一个JSX节点包含 '${directiveNames.ifDirective}' 指令.`);
+        } else if (elseDirective) {
+          throw nodePath.buildCodeFrameError(`'${elseDirective.name}' 指令需要上一个JSX节点包含 '${directiveNames.ifDirective}' 或 '${directiveNames.elifDirective}' 指令.`);
+        }
+
+        // 处理class指令
+        if (classDirective) {
+          if (classDirective.value === null) {
+            throw nodePath.get('openingElement').get(`attributes.${classDirective.key}`).buildCodeFrameError(`'${classDirective.name}' 指令需要绑定一个变量或表达式.`);
+          }
+          parseClassDirective(
+            attributes,
+            classDirective,
+            directiveNodes.classNameAttr,
+            classHelperAlias
+          );
+          removedNum = attributes.splice(classDirective.key - removedNum, 1);
+          state.__directiveHelpers.set(classHelperAlias, classHelper);
+        }
+
+        // 处理show指令
+        if (showDirective) {
+          if (showDirective.value === null) {
+            throw nodePath.get('openingElement').get(`attributes.${showDirective.key}`).buildCodeFrameError(`'${showDirective.name}' 指令需要绑定一个变量或表达式.`);
+          }
+          parseShowDirective(
+            attributes,
+            showDirective,
+            directiveNodes.styleAttr,
+            showHelperAlias
+          );
+          removedNum = attributes.splice(showDirective.key - removedNum, 1);
+          state.__directiveHelpers.set(showHelperAlias, showHelper);
+        }
+
+        // 处理html指令
+        if (htmlDirective) {
+          if (htmlDirective.value === null) {
+            throw nodePath.get('openingElement').get(`attributes.${htmlDirective.key}`).buildCodeFrameError(`'${htmlDirective.name}' 指令需要绑定一个变量或表达式.`);
+          }
+          parseHtmlDirective(
+            attributes,
+            htmlDirective,
+            directiveNodes.dangerouslySetInnerHTMLAttr
+          );
+          attributes.splice(htmlDirective.key - removedNum, 1);
+        }
+      }
+    }
+  };
+};
