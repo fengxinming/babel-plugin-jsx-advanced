9b94800dfee05dded34f5f30eb923a5f2424569c
Jesse Feng <fxm0016@126.com>
2020-10-22 22:48:16 +0800

    Create all.js

---
+++ packages/eslint-plugin-jsx-advanced/configs/all.js
@@ -0,0 +1,14 @@
+  
+'use strict';
+
+module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['jsx-advanced'],
+  rules: {
+    'jsx-advanced/jsx-uses-i-for': 2
+  }
+};
